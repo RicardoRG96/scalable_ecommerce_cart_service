f0dd98b39e02f3d58af3485fda9612c3a77179b6
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-27 05:31:16 -0300

    add Cart entity

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/cart_service/entities/Cart.java
@@ -0,0 +1,80 @@
+package com.ricardo.scalable.ecommerce.platform.cart_service.entities;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import com.ricardo.scalable.ecommerce.platform.libs_common.entities.User;
+
+@Entity
+@Table(name = "cart")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @CreationTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    public Cart() {
+    }
+
+    public Cart(Long id, User user, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.user = user;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
