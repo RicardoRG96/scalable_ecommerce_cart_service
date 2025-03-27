f5e734f3bca5f91e6ed3d518d7c6581032f6e1e1
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-27 06:04:48 -0300

    add CartItemRepository

---
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/cart_service/repositories/CartItemRepository.java
@@ -0,0 +1,15 @@
+package com.ricardo.scalable.ecommerce.platform.cart_service.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ricardo.scalable.ecommerce.platform.cart_service.entities.CartItem;
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemRepository extends CrudRepository<CartItem, Long> {
+
+    Optional<List<CartItem>> findByCartId(Long cartId);
+
+    Optional<List<CartItem>> findByProductSkuId(Long productSkuId);
+
+}
--- src/main/java/com/ricardo/scalable/ecommerce/platform/cart_service/repositories/CartRepository.java
+++ src/main/java/com/ricardo/scalable/ecommerce/platform/cart_service/repositories/CartRepository.java
@@ -8,6 +8,6 @@ import com.ricardo.scalable.ecommerce.platform.cart_service.entities.Cart;
 
 public interface CartRepository extends CrudRepository<Cart, Long> {
 
-    Optional<Cart> findByUserId();
+    Optional<Cart> findByUserId(Long userId);
 
 }
