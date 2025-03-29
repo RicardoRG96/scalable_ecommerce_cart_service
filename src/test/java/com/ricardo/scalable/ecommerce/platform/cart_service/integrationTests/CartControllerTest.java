410215d830155b6319b439afa98e9342d0239abc
RicardoRG96 <ricardoretamal10@gmail.com>
2025-03-29 08:42:48 -0300

    add application-test.properties

---
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/cart_service/integrationTests/CartControllerTest.java
@@ -0,0 +1,5 @@
+package com.ricardo.scalable.ecommerce.platform.cart_service.integrationTests;
+
+public class CartControllerTest {
+
+}
---
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/cart_service/integrationTests/testData/CartControllerTestData.java
@@ -0,0 +1,21 @@
+package com.ricardo.scalable.ecommerce.platform.cart_service.integrationTests.testData;
+
+import com.ricardo.scalable.ecommerce.platform.cart_service.repositories.dto.CartDto;
+
+public class CartControllerTestData {
+
+    public static CartDto createCartDto() {
+        CartDto cart = new CartDto();
+        cart.setUserId(1L);
+
+        return cart;
+    }
+
+    public static CartDto createCartDtoToUpdate() {
+        CartDto cart = new CartDto();
+        cart.setUserId(2L);
+
+        return cart;
+    }
+
+}
